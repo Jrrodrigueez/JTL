30f1880f7fe04de49d0c0460cff10b49181f881a
Jonathan Andres Rodriguez Delgado <jrodriguez518@misena.edu.co>
2019-11-25 20:19:06 -0500

    index primer cambio

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-    echo "works"
-?>
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Index Login</title>
+	</head>
+    
+	<body>
+        <h1>Please Login or SignUp</h1>
+        
+        <a href="login.php"</a> or
+        
+	</body>
+    
+</html>
