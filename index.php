8cf4fc290e149d8878fe72a29dd8144e84c0bba7
Jonathan Andres Rodriguez Delgado <jrodriguez518@misena.edu.co>
2019-11-25 20:59:11 -0500

    CSS and Fonts
    CSS and Fonts

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Index Login</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        
+        <link rel="stylesheet" href="assets/css/style.css">
+        
 	</head>
     
 	<body>
         <h1>Please Login or SignUp</h1>
         
-        <a href="login.php"</a> or
-        
+        <a href="login.php">Login</a> or
+        <a href="signup.php">Signup</a>
 	</body>
     
 </html>
